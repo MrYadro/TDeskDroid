800da6017ca8c52107c5d3eb33e62038e16780aa
Slim_d0g <slim_d0g@mail.ru>
2017-03-02 12:41:40 +0300

    move everythin to TDeskDroid class

--- main.py
+++ main.py
@@ -19,202 +19,202 @@ THEME_ALPHA_MAP_URL  = 'https://raw.githubusercontent.com/TThemes/TThemeMap/mast
 THEME_DEFAULT_PATH   = os.path.join(MAPS_DIR, 'default.map')
 THEME_DEFAULT_URL    = 'https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/Resources/colors.palette'
 OVERRIDE_MAP_PATH    = os.path.join(MAPS_DIR, 'override.map')
-TINIFY_KEY           = "API_KEY_HERE"
+TINIFY_KEY           = 'API_KEY_HERE'
 TINIFY_ENABLE        = False
 
 
+class TDeskDroid(object):
+    _defaultThemeMap = None
 
-def checkDirectories():
-    for directory in [DESKTOP_DIR, ANDROID_DIR, WIP_DIR, WIP_DROIDSRC_DIR]:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    def _checkDirectories(self):
+        for directory in [DESKTOP_DIR, ANDROID_DIR, WIP_DIR, WIP_DROIDSRC_DIR]:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
 
-def updateThemesMap():
-    print("Downloading [desktop => android] mapping")
-    themesMapContents = requests.get(THEME_MAP_URL)
-    with open(THEME_MAP_PATH, 'w') as themeMap:
-        themeMap.write(themesMapContents.text)
-    print("Downloading transparency fixups")
-    themesMapContents = requests.get(THEME_ALPHA_MAP_URL)
-    with open(THEME_ALPHA_MAP_PATH, 'w') as themeMap:
-        themeMap.write(themesMapContents.text)
-    print("Downloading default desktop theme")
-    themesMapContents = requests.get(THEME_DEFAULT_URL)
-    with open(THEME_DEFAULT_PATH, 'w') as themeMap:
-        themeMap.write(themesMapContents.text)
+    def _updateThemesMap(self):
+        print("Downloading [desktop => android] mapping")
+        themesMapContents = requests.get(THEME_MAP_URL)
+        with open(THEME_MAP_PATH, 'w') as themeMap:
+            themeMap.write(themesMapContents.text)
+        print("Downloading transparency fixups")
+        themesMapContents = requests.get(THEME_ALPHA_MAP_URL)
+        with open(THEME_ALPHA_MAP_PATH, 'w') as themeMap:
+            themeMap.write(themesMapContents.text)
+        print("Downloading default desktop theme")
+        themesMapContents = requests.get(THEME_DEFAULT_URL)
+        with open(THEME_DEFAULT_PATH, 'w') as themeMap:
+            themeMap.write(themesMapContents.text)
 
-def convertBackround(path, tinyJpeg):
-    filename = "background"
-    filetypes = ["jpg", "png"]
-    tries = 0
-
-    for filetype in filetypes:
-        try:
-            im = Image.open(os.path.join(WIP_DIR, path, filename + "." + filetype))
-        except FileNotFoundError:
-            tries += 1
-            pass
-
-    if tries == 2:
-        return False
-
-    jpg_path = os.path.join(WIP_DIR, path, JPEG_NAME)
-    im.save(jpg_path, "jpeg", quality=100)
-    if tinyJpeg:
-        import tinify
-        tinify.key = TINIFY_KEY
-        source = tinify.from_file(jpg_path)
-        source.to_file(jpg_path)
-    return True
-
-def normalizeColor(color):
-    color = color.lower()
-    if len(color) < 9:
-        color += "ff"
-    return color
-
-def substituteColor(rulesDict, color):
-    if color.startswith("#"):
-        return normalizeColor(color)
-    return substituteColor(rulesDict, rulesDict[color])
-
-def validateKeyValue(line, divider='='):
-    return (
-        line
-        and len(line) > 0
-        and divider in line
-        and (
-            line[0].isalpha() or line[0] == '_'
-        )
-    )
-
-def getKeyValue(line, divider='=', spliter='//'):
-    key, value = line.split(divider, 1)
-    key = key.strip()
-    value = value.split(spliter, 1)[0].strip()
-    return key, value
-
-def applyOverrideMap(overrideMapPath):
-    overrideDict = {}
-    if not os.path.exists(overrideMapPath):
-        return overrideDict
-    print("Applying '{}' override map".format(overrideMapPath))
-    with open(overrideMapPath, "r") as overrideMap:
-        for line in overrideMap:
-            if not validateKeyValue(line):
-                continue
-            name, color = getKeyValue(line)
-            if color.startswith("#"):
-                color = normalizeColor(color)
-            overrideDict[name] = color
-    return overrideDict
-
-defaultThemeMap = None
-def getDefaultThemeMap():
-    global defaultThemeMap
-    if defaultThemeMap is None:
-        defaultThemeMap = {}
-        themeDefault = open(THEME_DEFAULT_PATH, "r").readlines()
-        for line in themeDefault:
-            if not validateKeyValue(line, ':'):
-                continue
-            name, color = getKeyValue(line, ':', ';')
-            defaultThemeMap[name] = color
-    return defaultThemeMap
-
-def getDefaultColor(name):
-    return getDefaultThemeMap()[name]
-
-def makeAtthemeSrc(filedir, filename):
-    src = open(os.path.join(WIP_DIR, filename, "colors.tdesktop-theme"), "r").readlines()
-    themeMap = open(THEME_MAP_PATH, "r").readlines()
-    themeAlphaMap = open(THEME_ALPHA_MAP_PATH, "r").readlines()
-
-    # read original theme file
-    srcrules = {}
-    for line in src:
-        if not validateKeyValue(line, ':'):
-            continue
-        name, color = getKeyValue(line, ':', ';')
-        srcrules[name] = color
-
-    overrideMap = applyOverrideMap(OVERRIDE_MAP_PATH)
-    srcrules.update(overrideMap)
-
-    # substitute color values
-    for rule, color in srcrules.items():
-        srcrules[rule] = substituteColor(srcrules, color)
-
-    # parse theme.map
-    destrules = dict()
-    for line in themeMap:
-        if not validateKeyValue(line):
-            continue
-        name, color = getKeyValue(line)
-        while True:
+    def _convertBackround(self, path, tinyJpeg):
+        filename = "background"
+        filetypes = ["jpg", "png"]
+        tries = 0
+        for filetype in filetypes:
             try:
-                destrules[name] = substituteColor(srcrules, color)
-                break
-            except KeyError:
-                try:
-                    color = getDefaultColor(color)
-                except KeyError:
-                    print("Warning: couldn't find '{}' value. Using default color for '{}'.".format(color, name))
+                im = Image.open(os.path.join(WIP_DIR, path, filename + "." + filetype))
+            except FileNotFoundError:
+                tries += 1
+                pass
+        if tries == 2:
+            return False
+        jpg_path = os.path.join(WIP_DIR, path, JPEG_NAME)
+        im.save(jpg_path, "jpeg", quality=100)
+        if tinyJpeg:
+            import tinify
+            tinify.key = TINIFY_KEY
+            source = tinify.from_file(jpg_path)
+            source.to_file(jpg_path)
+        return True
 
-    for line in themeAlphaMap:
-        if not validateKeyValue(line):
-            continue
-        name, alpha = getKeyValue(line)
-        color = destrules[name]
-        destrules[name] = color[:7] + alpha
+    def _normalizeColor(self, color):
+        color = color.lower()
+        if len(color) < 9:
+            color += "ff"
+        return color
 
-    # apply overrides from map files
-    destrules.update(overrideMap)
-    overrideMapPath = filedir + os.path.sep
-    for filepart in os.path.basename(filename).split("."):
-        overrideMapPath += filepart + "."
-        overrideMap = applyOverrideMap(overrideMapPath + "map")
-        destrules.update(overrideMap)
+    def _substituteColor(self, rulesDict, color):
+        if color.startswith("#"):
+            return self._normalizeColor(color)
+        return self._substituteColor(rulesDict, rulesDict[color])
 
-    # substitute color values for overrides
-    for rule, color in destrules.items():
-        destrules[rule] = substituteColor(destrules, color)
+    def _validateKeyValue(self, line, divider='='):
+        return (
+            line
+            and len(line) > 0
+            and divider in line
+            and (
+                line[0].isalpha() or line[0] == '_'
+            )
+        )
 
-    # write theme file
-    with open(os.path.join(WIP_DROIDSRC_DIR, filename + ".atthemesrc"), "w") as themesrc:
-        for rule, color in destrules.items():
-            themesrc.write(rule + "=" + color + "\n")
+    def _getKeyValue(self, line, divider='=', spliter='//'):
+        key, value = line.split(divider, 1)
+        key = key.strip()
+        value = value.split(spliter, 1)[0].strip()
+        return key, value
 
-def makeAttheme(filename, hasBg):
-    src = open(os.path.join(WIP_DROIDSRC_DIR, filename + ".atthemesrc"), "r").readlines()
-    with open(os.path.join(ANDROID_DIR, filename + ANDROID_EXT), "wb") as theme:
+    def _applyOverrideMap(self, overrideMapPath):
+        overrideDict = {}
+        if not os.path.exists(overrideMapPath):
+            return overrideDict
+        print("Applying '{}' override map".format(overrideMapPath))
+        with open(overrideMapPath, "r") as overrideMap:
+            for line in overrideMap:
+                if not self._validateKeyValue(line):
+                    continue
+                name, color = self._getKeyValue(line)
+                if color.startswith("#"):
+                    color = self._normalizeColor(color)
+                overrideDict[name] = color
+        return overrideDict
+
+    @property
+    def defaultThemeMap(self):
+        if self._defaultThemeMap is None:
+            self._defaultThemeMap = {}
+            themeDefault = open(THEME_DEFAULT_PATH, "r").readlines()
+            for line in themeDefault:
+                if not self._validateKeyValue(line, ':'):
+                    continue
+                name, color = self._getKeyValue(line, ':', ';')
+                self._defaultThemeMap[name] = color
+        return self._defaultThemeMap
+
+    def _makeAtthemeSrc(self, filedir, filename):
+        src = open(os.path.join(WIP_DIR, filename, "colors.tdesktop-theme"), "r").readlines()
+        themeMap = open(THEME_MAP_PATH, "r").readlines()
+        themeAlphaMap = open(THEME_ALPHA_MAP_PATH, "r").readlines()
+
+        # read original theme file
+        srcrules = {}
         for line in src:
-            name, color = line.strip().split("=")
-            swappedColor = color[-2:] + color[1:7]
-            theme.write((name + "=#" + swappedColor + "\n").encode())
+            if not self._validateKeyValue(line, ':'):
+                continue
+            name, color = self._getKeyValue(line, ':', ';')
+            srcrules[name] = color
 
-        if hasBg:
-            theme.write("WPS\n".encode())
-            img = open(os.path.join(WIP_DIR, filename, JPEG_NAME), "rb")
-            theme.write(img.read())
-            theme.write("\nWPE".encode())
+        overrideMap = self._applyOverrideMap(OVERRIDE_MAP_PATH)
+        srcrules.update(overrideMap)
 
-checkDirectories()
-updateThemesMap()
+        # substitute color values
+        for rule, color in srcrules.items():
+            srcrules[rule] = self._substituteColor(srcrules, color)
 
-filedir = DESKTOP_DIR
-for file in os.listdir(filedir):
-    if file.endswith(DESKTOP_EXT):
-        filepath = os.path.join(filedir, file)
-        filename = os.path.splitext(file)[0]
-        with zipfile.ZipFile(os.path.join(DESKTOP_DIR, file),"r") as zip_ref:
-            print ("Converting '" + filename + "'")
-            zip_ref.extractall(os.path.join(WIP_DIR, filename))
-            hasBg = convertBackround(filename, TINIFY_ENABLE)
-            makeAtthemeSrc(filedir, filename)
-            makeAttheme(filename, hasBg)
+        # parse theme.map
+        destrules = dict()
+        for line in themeMap:
+            if not self._validateKeyValue(line):
+                continue
+            name, color = self._getKeyValue(line)
+            while True:
+                try:
+                    destrules[name] = self._substituteColor(srcrules, color)
+                    break
+                except KeyError:
+                    try:
+                        color = self.defaultThemeMap[color]
+                    except KeyError:
+                        print("Warning: couldn't find '{}' value. Using default color for '{}'.".format(color, name))
 
-print ("""Converton done\n
-If you have any bugs feel free to contact me:
-https://t.me/TDeskDroid
-https://github.com/MrYadro/TDeskDroid/issues/new""")
+        for line in themeAlphaMap:
+            if not self._validateKeyValue(line):
+                continue
+            name, alpha = self._getKeyValue(line)
+            color = destrules[name]
+            destrules[name] = color[:7] + alpha
+
+        # apply overrides from map files
+        destrules.update(overrideMap)
+        overrideMapPath = filedir + os.path.sep
+        for filepart in os.path.basename(filename).split("."):
+            overrideMapPath += filepart + "."
+            overrideMap = self._applyOverrideMap(overrideMapPath + "map")
+            destrules.update(overrideMap)
+
+        # substitute color values for overrides
+        for rule, color in destrules.items():
+            destrules[rule] = self._substituteColor(destrules, color)
+
+        # write theme file
+        with open(os.path.join(WIP_DROIDSRC_DIR, filename + ".atthemesrc"), "w") as themesrc:
+            for rule, color in destrules.items():
+                themesrc.write(rule + "=" + color + "\n")
+
+    def _makeAttheme(self, filename, hasBg):
+        src = open(os.path.join(WIP_DROIDSRC_DIR, filename + ".atthemesrc"), "r").readlines()
+        with open(os.path.join(ANDROID_DIR, filename + ANDROID_EXT), "wb") as theme:
+            for line in src:
+                name, color = line.strip().split("=")
+                swappedColor = color[-2:] + color[1:7]
+                theme.write((name + "=#" + swappedColor + "\n").encode())
+
+            if hasBg:
+                theme.write("WPS\n".encode())
+                img = open(os.path.join(WIP_DIR, filename, JPEG_NAME), "rb")
+                theme.write(img.read())
+                theme.write("\nWPE".encode())
+
+    def _printCredits(self):
+        print(
+            "If you have any bugs feel free to contact me:\n"
+            "https://t.me/TDeskDroid\n"
+            "https://github.com/MrYadro/TDeskDroid/issues/new\n"
+        )
+
+    def convert(self):
+        self._checkDirectories()
+        self._updateThemesMap()
+        filedir = DESKTOP_DIR
+        for file in os.listdir(filedir):
+            if file.endswith(DESKTOP_EXT):
+                filename = os.path.splitext(file)[0]
+                with zipfile.ZipFile(os.path.join(DESKTOP_DIR, file),"r") as zip_ref:
+                    print ("> Converting '" + filename + "'")
+                    zip_ref.extractall(os.path.join(WIP_DIR, filename))
+                    hasBg = self._convertBackround(filename, TINIFY_ENABLE)
+                    self._makeAtthemeSrc(filedir, filename)
+                    self._makeAttheme(filename, hasBg)
+        print("Converton done\n")
+        self._printCredits()
+
+TDeskDroid().convert()
